44ee89f1dcab91dcac1bdff030118a4b9313b4e9
Ting Zhou <limtingzhou@gmail.com>
2019-10-21 23:51:12 +0800

    update App.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MovingCircle from "./src/MovingCircle";
+import MovingCircleGoesToOriginalPosition from "./src/MovingCircleGoesToOriginalPosition";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MovingCircle />
+      <MovingCircleGoesToOriginalPosition />
     </View>
   );
 }
